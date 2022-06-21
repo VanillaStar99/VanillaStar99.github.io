56065f970dd4ea00e90837c1c3cf5b7de011cea5
Vanilla <86117675+VanillaStar99@users.noreply.github.com>
2022-06-21 17:45:40 +0800

    Add files via upload

---
+++ js/observer.js
@@ -0,0 +1,20 @@
+const fadeItems = document.querySelectorAll(".fade");
+
+const fadeinOption = {
+  rootMargin: "0px 0px -175px 0px"
+};
+
+const fadeinScroll = new IntersectionObserver(function (items, fadeinScroll) {
+  items.forEach(item => {
+    if (!item.isIntersecting) {
+      return;
+    } else {
+      item.target.classList.add("fadein");
+      fadeinScroll.unobserve(item.target);
+    }
+  });
+}, fadeinOption);
+
+fadeItems.forEach(fadeItem => {
+  fadeinScroll.observe(fadeItem);
+});
