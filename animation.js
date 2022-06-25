ceed1cd1e7d9274e22fbe41c47ded08b537bba6d
Vanilla <86117675+VanillaStar99@users.noreply.github.com>
2022-06-25 11:09:37 +0800

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,35 @@
+var fadeItems = document.querySelectorAll(".fade");
+var header = document.querySelector("header");
+var toTop = document.querySelector(".to-top")
+
+document.oncontextmenu = function () {
+  return false;
+}
+
+const fadeinOption = {
+  rootMargin: "0px 0px -100px 0px"
+};
+
+const fadeinScroll = new IntersectionObserver(function (items) {
+  items.forEach(item => {
+    if (!item.isIntersecting) {
+      return;
+    } else {
+      item.target.classList.add("fadein");
+    }
+  });
+}, fadeinOption);
+
+fadeItems.forEach(fadeItem => {
+  fadeinScroll.observe(fadeItem);
+});
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 120) {
+    header.classList.add("header-colour");
+    toTop.classList.add("to-top-show");
+  } else {
+    header.classList.remove("header-colour");
+    toTop.classList.remove("to-top-show");
+  }
+})
